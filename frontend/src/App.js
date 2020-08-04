71fc4c3478d6c1153faf7c6544f155bc44720f76
Paweł Karaś <pwkaras@gmail.com>
2020-08-04 18:55:59 +0200

    frontend routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
+import Users from './user/views/Users';
+import NewPlaces from './places/views/NewPlaces';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlaces />
+      </Route>
+      <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
